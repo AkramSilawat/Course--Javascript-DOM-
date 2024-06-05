e9502f328fa923c7c48ea70954e835c48bcb6dfa
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-06-05 10:47:02 +0530

    GetBoundingClientect

--- 11-getBoundingClientRect.js
+++ 11-getBoundingClientRect.js
@@ -6,7 +6,11 @@ console.log(rect);
 
 
 window.addEventListener('scroll', function () {
+console.log(box.getBoundingClientRect().top,window.innerHeight);
+
     if (box.getBoundingClientRect().top < window.innerHeight) {
       box.style.background = 'red';
     }
 });
+
+
